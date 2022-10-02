37252170945a71edaaaaf14789d96785ac3a3f60
Terrence Alsup <terrencealsup@Terrences-MacBook-Air.local>
2022-10-02 10:17:48 -0400

    add batch sampler for reference computation

---
+++ computeBatchSamples.m
@@ -0,0 +1,18 @@
+function [] = computeBatchSamples(jobnum)
+% computeBatchSamples draws a batch of samples
+
+addpath(genpath('FOM/'));
+
+level = 4;
+M = 1e+3;
+Y = drawSamples(M);
+o = zeros(M, 1);
+for i=1:M
+    disp(num2str(i));
+    o(i) = beamFOM(level, Y(i, :));
+end
+file_name = sprintf('results/batchSamples/batchSamples-%d.mat', jobnum);
+save(file_name, 'Y', 'o', 'level', 'M', '-v7.3');
+
+end
+
--- estimateQOI.m
+++
@@ -1,6 +0,0 @@
-function qoi = estimateQOI(o, z0)
-%estimateQOI estimate quantity of interest Pr(o <= z0) with samples Y.
-
-qoi = mean(limitg(o, z0));
-end
-
